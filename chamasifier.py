849eb165c0f0dea663b60200fe323643670e6495
xidhu <sidhu3612@gmail.com>
2021-05-16 20:12:03 +0530

    error corrected

--- chamasifier.py
+++ chamasifier.py
@@ -1,11 +1,12 @@
-#Importing necessary libraries and packages
+# Importing necessary libraries and packages
 
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.layers import MaxPooling2D
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import MaxPooling2D
 
 # Create a neural network model
 
@@ -27,45 +28,50 @@ model.add(Flatten())
 # Add 5 dense hidden layers
 # Add one dense output layer for the 2 photos
 
-model.add(Dense(units=32,activation = 'relu'))
-model.add(Dense(units=64,activation = 'relu'))
-model.add(Dense(units=128,activation = 'relu'))
-model.add(Dense(units=256,activation = 'relu'))
-model.add(Dense(units=256,activation = 'relu'))
-model.add(Dense(units=2,activation = 'sigmoid'))
+model.add(Dense(units=32, activation='relu'))
+model.add(Dense(units=64, activation='relu'))
+model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=256, activation='relu'))
+model.add(Dense(units=256, activation='relu'))
+model.add(Dense(units=2, activation='sigmoid'))
 
-#Compiling CNN
-model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+# Compiling CNN
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-#Fitting CNN to images
+# Fitting CNN to images
 
-from keras.preprocessing.image import ImageDataGenerator
-train_datagen = ImageDataGenerator(rescale = 1./255, # To rescaling the image in range of [0,1]
-                                   shear_range = 0.2, # shear the images from current position
-                                   zoom_range = 0.2, # To randomly zoom the images
-                                   horizontal_flip = True) #  for randomly flipping half of the images horizontally
+train_datagen = ImageDataGenerator(rescale=1./255,  # To rescaling the image in range of [0,1]
+                                   shear_range=0.2,  # shear the images from current position
+                                   zoom_range=0.2,  # To randomly zoom the images
+                                   horizontal_flip=True)  # for randomly flipping half of the images horizontally
 
 #Testing and training
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 print("\nTraining and testing the data...\n")
 training_set = train_datagen.flow_from_directory('train',
-                                                target_size=(64,64),
-                                                batch_size=6, #Total no. of batches
-                                                class_mode='binary')
+                                                 save_format='jpg',
+                                                 target_size=(64, 64),
+                                                 class_mode='categorical',
+                                                 batch_size=12,  # Total no. of batches
+                                                )
 test_set = test_datagen.flow_from_directory('test',
-                                            target_size=(64,64),
+                                            target_size=(64, 64),
+                                            save_format='jpg',
+                                            class_mode='categorical',
                                             batch_size=12,
-                                            class_mode='binary')
+                                            )
 
-#training the model             error_here
+# training the model             error_here
 model.fit_generator(
-        training_set,
-        validation_steps=35,
-        epochs=5,
-        validation_data=test_set,
-        nb_val_samples=18)
+    training_set,
+    validation_steps=35,
+    epochs=20,
+    validation_data=test_set,
+)
 
-#Saving the model
+
+# Saving the model
 
 model.save("CNNmodel.h5")
---
+++ test.py
@@ -0,0 +1,15 @@
+from tensorflow.keras import models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+model = models.load_model("CNNmodel.h5")
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_datagen.flow_from_directory('test',
+                                            target_size=(64, 64),
+                                            save_format='jpg',
+                                            class_mode='categorical',
+                                            batch_size=12,
+                                            )
+result = model.predict_generator(test_set,verbose=1)
+
+print(result)
