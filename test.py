924768944faad2f4e3891959bb50211c18df7a41
Xidhu <64782756+xidhu@users.noreply.github.com>
2021-05-16 23:41:39 +0530

    Change

--- test.py
+++ test.py
@@ -5,12 +5,6 @@ import cv2
 
 model = models.load_model("CNNmodel.h5")
 
-model.compile(loss='binary_crossentropy',
-
-              optimizer='rmsprop',
-
-              metrics=['accuracy'])
-
 img = cv2.imread('tester/mango.jpg')
 
 img = cv2.resize(img,(64,64))
